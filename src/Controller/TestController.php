7944c1a5f923a3957d79b09570b0936fc0514638
Filip Horvat <fico7489@gmail.com>
2023-03-18 09:25:58 +0100

    update2

---
+++ src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'test')]
+    public function test()
+    {
+        exit('test...');
+    }
+}
